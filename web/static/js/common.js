4a98763cf4fbd04071c792329adf2741f5ccecc6
juliancoleman <julcol03@gmail.com>
2015-05-19 13:08:35 -0700

    require config should be working... I hope

---
+++ web/static/js/common.js
@@ -0,0 +1,55 @@
+require.config({
+    baseUrl: 'static/js',
+
+    paths: {
+        'marionette': 'vendor/backbone/marionette'
+    },
+
+    packages: [
+        {
+            location: 'app',
+            name: 'app'
+        },
+        {
+            location: 'jquery',
+            name: 'jquery',
+            main: 'jquery'
+        },
+        {
+            location: 'vendor/backbone',
+            name: 'backbone',
+            main: 'backbone'
+        },
+        {
+            location: 'vendor/hbs',
+            name: 'hbs',
+            main: 'hbs'
+        }
+    ],
+
+    map: {
+        '*': {
+            'underscore': 'vendor/underscore/lodash',
+            'handlebars': 'hbs/handlebars'
+        }
+    },
+
+    hbs: {
+        templateExtension: 'html',
+        disableI18n: true,
+        helperDirectory: 'app/shared/hbs'
+    },
+
+    shim: {
+        'backbone': {
+            'deps': ['jquery', 'underscore'],
+            'exports': 'Backbone'
+        },
+        'marionette': {
+            'deps': ['jquery', 'underscore', 'backbone'],
+            'exports': 'Marionette'
+        }
+    },
+
+    wrapShim: true,
+})
