a5ef3720c8ad2ef744d0ceb8681b2f0ecf658d64
juliancoleman <julcol03@gmail.com>
2015-05-19 13:24:47 -0700

    alright, app is looking good

---
+++ web/static/js/app/app.js
@@ -0,0 +1,29 @@
+define(function (require, exports, module) {
+    var Marionette = require('marionette')
+
+    var Ricetta = new Marionette.Application.extend({
+        initialize: function (options) {}
+    })
+
+    var app = new Ricetta()
+
+    app.addRegions({
+        mainRegion: '#mainRegion'
+        headerRegion: '#headerRegion'
+    })
+
+    app.addInitializer(function() {
+        Backbone.history.start({
+            pushState: false
+        })
+        $.ajaxSetup({
+            statusCode: {
+                401: function() {
+                    window.location.reload()
+                }
+            }
+        })
+    })
+
+    return app
+})
